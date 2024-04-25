b4a744dc100114df5ded85ae2fc886042a04f7b4
xriis <xriis@x7i>
2024-04-25 20:45:11 -0300

    punto 1 del tp2 en proceso

--- TP02_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio01/main/Main.java
+++ TP02_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio01/main/Main.java
@@ -1,9 +1,28 @@
 package ar.edu.unju.fi.ejercicio01.main;
 
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.ejercicio01.model.Producto;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Probando new brench");
+		
+		ArrayList<Producto> products = new ArrayList<Producto>();
+		
+		Producto p1 = new Producto();
+		
+		p1.setCode("AAA123");
+		p1.setDesc("Computer");
+		p1.setPrice(1234567f);
+		p1.setMadeIn(p1.getMadeIn().ARGENTINA);
+		p1.setCat(p1.getCat().INFORMATICA);
+		
+		products.add(p1);
+		
+		for(Producto p: products) {
+			System.out.println(p.toString());
+		}
 	}
 
 }
