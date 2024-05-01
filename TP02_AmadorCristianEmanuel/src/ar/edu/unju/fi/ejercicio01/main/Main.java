c35bbe1930b6e725929bc29d7a67cfbd414eda2c
xriis <xriis@x7i>
2024-04-30 21:08:36 -0300

    Probando cambios de enlaces del punto 1 a la rama principal [main]

--- TP02_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio01/main/Main.java
+++ TP02_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio01/main/Main.java
@@ -12,7 +12,6 @@ public class Main {
 	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
 		int o = 0;
 		ArrayList<Producto> products = new ArrayList<Producto>();
 
