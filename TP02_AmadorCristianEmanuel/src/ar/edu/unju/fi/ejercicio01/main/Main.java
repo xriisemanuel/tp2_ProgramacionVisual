7f8c35517bf75c91a3cdb518636332bb05bdbb63
xriis <xriis@x7i>
2024-04-30 21:02:38 -0300

    prueba de actualizacion del punto 1 en la rama principal

--- TP02_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio01/main/Main.java
+++ TP02_AmadorCristianEmanuel/src/ar/edu/unju/fi/ejercicio01/main/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		
 		int o = 0;
 		ArrayList<Producto> products = new ArrayList<Producto>();
 
